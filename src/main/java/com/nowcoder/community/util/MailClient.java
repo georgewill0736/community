72aac4d529b36ec4aeb97919afc32528f2b882d3
georgewill <750100157@qq.com>
2021-01-19 01:13:29 +0800

    @2.5登录模块

--- src/main/java/com/nowcoder/community/util/MailClient.java
+++ src/main/java/com/nowcoder/community/util/MailClient.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailMessage;
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -12,32 +14,32 @@ import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.Date;
 import java.util.logging.Logger;
 
 @Component
 public class MailClient {
 
-    private static final org.slf4j.Logger logger=LoggerFactory.getLogger(MailClient.class);
-
+   private static final org.slf4j.Logger logger = LoggerFactory.getLogger(MailClient.class);
 
     @Autowired
-    private JavaMailSender mailSender;
+    private JavaMailSender javaMailSender;
 
     @Value("${spring.mail.username}")
     private String from;
 
-    public void sendMail(String to,String subject,String content) {
+    public void sendMail(String to,String Subject,String Content) {
         try {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-            mimeMessageHelper.setFrom(from);
-            mimeMessageHelper.setTo(to);
-            mimeMessageHelper.setSubject(subject);
-            mimeMessageHelper.setText(content,true);
-            mailSender.send(mimeMessageHelper.getMimeMessage());
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setText(Content,true);
+            helper.setSubject(Subject);
+            javaMailSender.send(helper.getMimeMessage());
         } catch (MessagingException e) {
-            logger.error("发送邮件失败"+e.getMessage());
+            logger.error("发送邮件失败la哈哈哈哈"+e.getMessage());
         }
-    }
 
+    }
 }
