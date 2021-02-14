668d841bef022ed0102d49dd61e5b1a1ffa7af6d
georgewill <750100157@qq.com>
2021-02-14 22:21:41 +0800

    @4.5关注，取关

--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.Service.FollowService;
 import com.nowcoder.community.Service.LikeService;
 import com.nowcoder.community.Service.UserService;
 import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.apache.commons.lang3.StringUtils;
@@ -30,7 +32,7 @@ import java.io.OutputStream;
  */
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements CommunityConstant {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -51,6 +53,9 @@ public class UserController {
 
     @Autowired
     private LikeService likeService;
+
+    @Autowired
+    private FollowService followService;
 
     @LoginRequired
     @RequestMapping(path = "/setting",method = RequestMethod.GET)
@@ -130,6 +135,21 @@ public class UserController {
         //点赞数量
         int likeCount = likeService.findUserLikeCount(userId);
         model.addAttribute("likeCount",likeCount);
+
+        //关注数量
+        long followeeCount = followService.findFolloweeCount(userId,ENTITY_TYPE_USER);
+        model.addAttribute("followeeCount",followeeCount);
+
+        // 粉丝数量
+        long followerCount = followService.findFollowerCount(ENTITY_TYPE_USER,userId);
+        model.addAttribute("followerCount",followerCount);
+
+        // 是否已关注
+        boolean hasFollowed = false;
+        if (hostHolder.getUser()!=null) {
+            hasFollowed = followService.hasFollowed(hostHolder.getUser().getId(),ENTITY_TYPE_USER,userId);
+        }
+        model.addAttribute("hasFollowed",hasFollowed);
 
         return "/site/profile";
     }
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -12,6 +12,10 @@ public class RedisKeyUtil {
 
     private static final String PREFIX_USER_LIKE = "like:user";
 
+    private static final String PREFIX_FOLLOWEE = "followee";
+
+    private static final String PREFIX_FOLLOWER = "follower";
+
     //某个实体的赞
     //like:entity:entityType:entityId->set(userId)
     public static String getEntityLikeKey(int entityType,int entityId) {
@@ -22,6 +26,19 @@ public class RedisKeyUtil {
     //like:user:userId->int
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE+SPLIT+userId;
+    }
+
+
+    //某个用户关注的实体，now为当前系统时间，系统当前时间用来排序
+    //followee:userId:entityType->zset(entityId,now)
+    public static String getFolloweeKey(int userId,int entityType) {
+        return PREFIX_FOLLOWEE+SPLIT+userId+SPLIT+entityType;
+    }
+
+    //某个用户拥有的粉丝，now为当前系统时间
+    //follower:entityType:entityId->zset(userId,now)
+    public static String getFollowerKey(int entityType,int entityId) {
+        return PREFIX_FOLLOWER+SPLIT+entityType+SPLIT+entityId;
     }
 
 }
